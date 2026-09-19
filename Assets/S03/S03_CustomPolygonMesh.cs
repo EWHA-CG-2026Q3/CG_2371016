b53574f969e24c43a3249a4591fc3f175d3de3a6
srah1004 <srah1004@ewhain.net>
2026-09-20 00:12:08 +0900

    Implement custom polygon mesh

---
+++ Assets/S03/S03_CustomPolygonMesh.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class S03_CustomPolygonMesh : MonoBehaviour
+{
+    void Start()
+    {
+        Vector3[] vertices = new Vector3[]
+        {
+            new Vector3(0f, 1f, 0f),
+            new Vector3(-1f, 0.3f, 0f),
+            new Vector3(-0.6f, -0.8f, 0f),
+            new Vector3(0.6f, -0.8f, 0f),
+            new Vector3(1f, 0.3f, 0f)
+        };
+
+        int[] triangles = new int[]
+        {
+            0, 1, 2,
+            0, 2, 3,
+            0, 3, 4
+        };
+
+        Mesh mesh = new Mesh();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        GetComponent<MeshRenderer>().sharedMaterial =
+            new Material(Shader.Find("Universal Render Pipeline/Lit"));
+    }
+}
